24a672275011a0bede14fc60eb149ca689352d78
justngocminh <khuatduytien24@gmail.com>
2024-04-27 22:06:01 +0700

    update

--- footer.js
+++ footer.js
@@ -1,14 +1,17 @@
 window.addEventListener("scroll", function () {
 	var elements = document.querySelectorAll(".animate");
-
 	for (var i = 0; i < elements.length; i++) {
 		var element = elements[i];
 		var bounding = element.getBoundingClientRect();
-		var windowHeight =
-			window.innerHeight || document.documentElement.clientHeight;
-
 		if (bounding.top >= 0) {
 			element.classList.add("animated");
 		}
+	}
+});
+window.addEventListener("scroll", function () {
+	var main = this.document.querySelector(".main-nav");
+	var mainBounding = main.getBoundingClientRect();
+	if (mainBounding.top < 0) {
+		main.classList.add("fixed");
 	}
 });
