a0279f90a325a2b553dcf99f7cfb5eb1f177b49d
justngocminh <khuatduytien24@gmail.com>
2024-04-27 10:07:35 +0700

    update animation

---
+++ footer.js
@@ -0,0 +1,18 @@
+function slideInOnScroll() {
+	var elements = document.querySelectorAll(".animate");
+
+	for (var i = 0; i < elements.length; i++) {
+		var element = elements[i];
+		var bounding = element.getBoundingClientRect();
+		var windowHeight =
+			window.innerHeight || document.documentElement.clientHeight;
+
+		if (bounding.top >= 0) {
+			element.classList.add("animated");
+		}
+	}
+}
+
+window.addEventListener("scroll", slideInOnScroll);
+
+slideInOnScroll();
