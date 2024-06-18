a09b0f8e276c0b8ef75a60c0383897f9061e9e77
justngocminh <nngocm777@gmail.com>
2024-06-18 21:53:44 +0700

    update header footer

---
+++ header-footer-comp/header-and-footer.js
@@ -0,0 +1,22 @@
+// footer animation 
+window.addEventListener("scroll", function () {
+	var elements = document.querySelectorAll(".animate");
+	for (var i = 0; i < elements.length; i++) {
+		var element = elements[i];
+		var bounding = element.getBoundingClientRect();
+		if (bounding.top >= 0) {
+			element.classList.add("animated");
+		}
+	}
+});
+
+// nav nav animation
+function hamburgerClicked() {
+	const navnav = document.querySelector(".nav-nav");
+	navnav.classList.toggle("active");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+	var hamburger = document.querySelector(".hamburger");
+	hamburger.addEventListener("click", hamburgerClicked);
+});
